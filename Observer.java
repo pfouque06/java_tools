3e588e3916f553106492d25935d26b6baeab8028
pfouque06 <pfouque@gmail.com>
2020-01-02 00:26:46 +0100

    b0.20.02 -observer.java : swap to generic LinkedHashSet<String[]> objet for update request from Observer

--- Observer.java
+++ Observer.java
@@ -1,9 +1,16 @@
 package java_tools;
 
+import java.util.LinkedHashSet;
+
 public interface Observer {
 
-	  public void update(String pString);
+	// Interface implemented by Observer in order to receive an update request from Observed class
+	// LinkedHashSet<String[]> pLHS is genric and can be used as suited
+	// for instance :
+	// - getOpts : String[] buffer = { type, key, valueName, action };
+	//
+	// idea : change terminology to transmitter / receiver ???
+	//
+	public void update(LinkedHashSet<String[]> pLHS);
 	  
-	  public void update(boolean pBoolean);
-	  //public void update(Object pObject);
 }
