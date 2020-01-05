9e09538adfa2a2d52a55607018a623588c91e715
pfouque06 <pfouque@gmail.com>
2020-01-06 00:39:59 +0100

    b0.20.04 -getOpts: improve comments

--- getOpts.java
+++ getOpts.java
@@ -11,8 +11,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
+/*
 // getOpts class to parse Command Lien arguments
 // must be used with an option Table such as :
+//
 // static String[] optionArray = {
 //		"##### DO NOT FORGET FOLLOWING HEADER LINE !! #####",
 //		"TYPE:KEY:KEYWORD:VALUENAME:VALUETYPE:DETAIL:ACTION:",
@@ -28,28 +30,111 @@ import java.util.LinkedList;
 //		"V:g:gain:gainMax:int:set maximum total gain to quit game:-:",
 //		"V:b:bet:betMax:int:set maximum bets allowed per tour:-:",
 //		};
+// 	public static getOpts options = new getOpts(optionArray);
+//
+//	public static boolean setOpts(String[] pArgs) {
+//		//System.out.println("optionTable=\n"+options.optionTable_toString());
+//		//parse options pArgs
+//		if (!options.setOptionList(pArgs)) {
+//			System.out.println("Parsing error, please retry or use -h, --help to get usage ...");
+//			return false;
+//		}
+//		//System.out.println("optionList=\n"+options.optionList_toString());
+//		// set options
+//		if (!setOpts(options.getOptionList())) {
+//		options.getUsage(System.out); // use STDOUT when help is requested
+//		return false;
+//		}
+//		//System.out.println("options:" + optsToString());
+//		return true;
+//	}
+//
+//	public static boolean setOpts(LinkedHashSet<String[]> pList) {
+//		// Loop on each options of pList
+//		for (String[] fields : pList) {
+//			//System.out.println("fields="+fields.toString() );
+//			switch (fields[2]) {
+//			case "colorMode":
+//				colorMode = fields[3].equals("true");
+//				// System.out.println("colorMode=" + colorMode);
+//				break;
+//			case "autoMode":
+//				autoMode = fields[3].equals("true");
+//				// System.out.println("auto=" + auto);
+//				break;
+//			case "deposit":
+//				deposit += Integer.valueOf(fields[3]);
+//				// System.out.println("deposit=" + deposit);
+//				break;
+//			case "jetonWarning":
+//				warning = Integer.valueOf(fields[3]);
+//				// System.out.println("warning=" + warning);
+//				break;
+//			case "jetonMax":
+//				jetonLimite = Integer.valueOf(fields[3]);
+//				// System.out.println("jetonLimite=" + jetonLimite);
+//				break;
+//			case "phaseMax":
+//				phaseMax = Integer.valueOf(fields[3]);
+//				// System.out.println("phaseMax=" + phaseMax);
+//				break;
+//			case "tourMax":
+//				tourMax = Integer.valueOf(fields[3]);
+//				// System.out.println("tourMax=" + tourMax);
+//				break;
+//			case "gainMax":
+//				gainMax = Integer.valueOf(fields[3]);
+//				// System.out.println("gainMax=" + gainMax);
+//				break;
+//			case "betMax":
+//				betMax = Integer.valueOf(fields[3]);
+//				// System.out.println("betMax=" + betMax);
+//				break;
+//			default:
+//				System.err.println("Error: option > valuename unknown");
+//			case "usage":
+//				return false;
+//			}
+//		}
+//		if (warning == 0)
+//			warning = deposit / 2;
+//		return true;
+//	}
+//
+//	public static String optsToString() {
+//
+//		String buffer = "";
+//
+//		buffer += " auto: " + (autoMode ? c_green() + "ON" : c_red() + "OFF") + c_reset();
+//		buffer += " mode: " + (colorMode ? c_green() + "color" : c_red() + "mono") + c_reset();
+//		if (deposit > 0)
+//			buffer += " deposit: " + c_blue() + deposit + c_reset();
+//		if (warning > 0)
+//			buffer += " warning: " + c_blue() + warning + c_reset();
+//		if (jetonLimite > 0)
+//			buffer += " jetonLimite: " + c_blue() + jetonLimite + c_reset();
+//		if (betMax > 0)
+//			buffer += " betMax: " + c_blue() + betMax + c_reset();
+//		if (gainMax > 0)
+//			buffer += " gainMax: " + c_blue() + gainMax + c_reset();
+//		if (phaseMax > 0)
+//			buffer += " phaseMax: " + c_blue() + phaseMax + c_reset();
+//		if (tourMax > 0)
+//			buffer += " toursMax: " + c_blue() + tourMax + c_reset();
+//		return buffer;
+//	}
 //
 //	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
 //
 //		// parse args :
-//		// initiate getOpts class and parse args according to file getOptsTable.txt
-//		// (default)
-//		// getOpts options = new getOpts();
-//		// //System.out.println("optionTable=\n"+options.optionTable_toString());
-//
-//		getOpts options = new getOpts(optionArray); // System.out.println("optionTable=\n"+options.optionTable_toString());
-//		if (!options.setOptionList(args)) {
-//			options.getUsage(System.out); // use STDOUT when help is requested
+//		//initiate getOpts class and parse args according to optionArray
+//		if (!setOpts(args))
 //			return;
-//		}
 //
-//		// System.out.println("optionList=\n"+options.optionList_toString());
-//		if (!setOpts(options.getOptionList())) {
-//			options.getUsage(System.out); // use STDOUT when help is requested
-//			return;
-//		}
+//		...
 //	}
+*/
 
 public class getOpts {
 
@@ -375,9 +460,9 @@ public class getOpts {
 		String result = "";
 		for (String[] fields : optionList) {
 			// System.out.println("fields="+fields.toString() );
+			result += (result.isEmpty() ? "" : "\n");
 			for (String cell : fields)
 				result += cell + ":";
-			result += "\n";
 		}
 		return result;
 	}
