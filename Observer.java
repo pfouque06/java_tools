e718dc20287c2cf837c8d84f4e358bf171124ed3
pfouque06 <pfouque@gmail.com>
2020-01-28 02:05:40 +0100

    b0.21.03 -improve GetOpts

--- Observer.java
+++ Observer.java
@@ -1,16 +1,18 @@
 package javaTools;
 
+//import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 public interface Observer {
 
 	// Interface implemented by Observer in order to receive an update request from Observed class
-	// LinkedHashSet<String[]> pLHS is genric and can be used as suited
+	// LinkedHashSet<String[]> pLHS is generic and can be used as suited
 	// for instance :
 	// - getOpts : String[] buffer = { type, key, valueName, action };
 	//
 	// idea : change terminology to transmitter / receiver ???
 	//
+	//public void update(LinkedHashMap<String, Object> pLHS);
 	public void update(LinkedHashSet<String[]> pLHS);
 	  
 }
